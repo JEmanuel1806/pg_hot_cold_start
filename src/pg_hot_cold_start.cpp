271c15fb4023b60d9d3646851de922b3977271a3
Jan Emanuél <33660472+JEmanuel1806@users.noreply.github.com>
2024-06-24 18:07:02 +0200

    Update pg_hot_cold_start.cpp

--- src/pg_hot_cold_start.cpp
+++ src/pg_hot_cold_start.cpp
@@ -1,4 +1,5 @@
 #include "postgres.h"
+#include "utils/guc.h"
 #include "fmgr.h"
 #include "executor/executor.h"
 #include "utils/rel.h"
@@ -12,51 +13,76 @@
 PG_MODULE_MAGIC;
 #endif
 
-// needed hooks
+/* GUC Variable */
+static char *experiment_mode = NULL;
+
+/* Used Hook */
 static ExecutorRun_hook_type pg_exec_run_prev_hook = NULL;
-// Traverse query plan tree and get all relevant relations
-static List *get_relations_from_plan(QueryDesc *queryDesc)
+
+/* Traverse query plan tree and get all relevant relations */
+static void get_relations_from_plan(QueryDesc *queryDesc, Plan *planTree, List **oidList)
 {
-    // Plan *plan = queryDesc->plannedstmt->planTree;
     RangeTblEntry *rte;
-
-    Relation relation = NULL;
-    char *relName = NULL;
     Oid relOid;
+    Oid indexOid;
+    Oid bitOid;
 
-    List *oidList = NIL;
-
-    for (int i = 0; i < list_length(queryDesc->plannedstmt->rtable); i++)
+    /* Check if planTree is empty to check if tree traversed (recursion) */
+    if (planTree == NULL)
     {
-        rte = list_nth(queryDesc->plannedstmt->rtable, i);
-        if (rte->rtekind == RTE_RELATION)
-        {
-            relation = RelationIdGetRelation(rte->relid);
-            relName = NameStr(relation->rd_rel->relname);
-            relOid = RelationGetRelid(relation);
-
-            elog(INFO, "Hook Test! Relation OID : %u", relOid);
-            elog(INFO, "Hook Test! Relation Name : %s", relName);
-
-            RelationClose(relation);
-
-            oidList = lappend_oid(oidList, relOid);
-        }
+        return;
     }
 
-    return oidList;
+    /*
+     * Traverse each node in the plan tree and check whether its a relation or an index
+     *
+     * For relations, check if node represents a sequential scan. If so, retrieve the
+     * relOid and stores it in a list (oidList).
+     *
+     * For indexes, check for the type of index and directly store its ID in the oidList, since
+     * no RangeTable indirection.
+     *
+     * The oidList is later passed to the prewarm function for further processing.
+     */
+
+    if (planTree->type == T_SeqScan)
+    {
+        Scan *scan = (Scan *)planTree;
+        rte = list_nth(queryDesc->plannedstmt->rtable, scan->scanrelid - 1);
+        relOid = rte->relid;
+        elog(INFO, "Scanning relation with OID: %d", relOid);
+        *oidList = lappend_oid(*oidList, relOid);
+    }
+    else if (planTree->type == T_IndexScan)
+    {
+        IndexScan *indexScan = (IndexScan *)planTree;
+        elog(INFO, "Scanning index with OID: %d", indexScan->indexid);
+        indexOid = indexScan->indexid;
+        *oidList = lappend_oid(*oidList, indexOid);
+    }
+    else if (planTree->type == T_BitmapIndexScan)
+    {
+        BitmapIndexScan *bitmapIndexScan = (BitmapIndexScan *)planTree;
+        elog(INFO, "Scanning bitmap index with OID: %d", bitmapIndexScan->indexid);
+        bitOid = bitmapIndexScan->indexid;
+        *oidList = lappend_oid(*oidList, bitOid);
+    }
+
+    /* Recursively walk through the tree and its children, pass the List */
+    get_relations_from_plan(queryDesc, planTree->lefttree, oidList);
+    get_relations_from_plan(queryDesc, planTree->righttree, oidList);
 }
 
 static void prewarm_relations(unsigned int relOid)
 
 {
-    elog(INFO, "Prewarm Called! Relation OID : %u", relOid);
 
     Relation rel;
     BlockNumber blkno;
     BlockNumber nblocks;
     Buffer buf;
-    
+
+    elog(INFO, "Prewarming Relation/Index with OID : %u", relOid);
 
     // Open the relation
     rel = relation_open(relOid, AccessShareLock);
@@ -77,12 +103,8 @@ static void prewarm_relations(unsigned int relOid)
         ReleaseBuffer(buf);
     }
 
-    elog(INFO, "Prewarmed all blocks in relation");
-
     // Close the relation
     relation_close(rel, AccessShareLock);
-
-    elog(INFO, "Closed relation with OID: %u", relOid);
 }
 
 /*
@@ -91,22 +113,46 @@ static void empty_buffer()
 }
 */
 
-// Actual extension starting point
+/* Actual extension starting point */
 static void pg_query_pre_run_hook(QueryDesc *queryDesc, ScanDirection direction, uint64 count, bool execute_once)
 {
-    // Determine relevant relations via OID directly from query plan
-    List *relations = get_relations_from_plan(queryDesc);
+    Plan *planTree = queryDesc->plannedstmt->planTree;
+    List *oidList = NIL;
 
-    // Check GUC
+    /*
+     * HOT START
+     * Get the relations and indices from the query plan tree and prewarm each of them
+     */
 
-    // Load relations into buffer
-    for (int i = 0; i < list_length(relations); i++)
+    if (strcmp(experiment_mode, "hot") == 0)
     {
-        prewarm_relations(list_nth_oid(relations, i));
+        /* Get the relations to be prewarmed out of the query plan */
+        get_relations_from_plan(queryDesc, planTree, &oidList);
+
+        /* Check content of OID List */
+        if (list_length(oidList) != 0)
+        {
+            for (int i = 0; i < list_length(oidList); i++)
+            {
+                elog(INFO, "List Content OID: %d", list_nth_oid(oidList, i));
+            }
+        }
+        /* Iterate over OIDList and prewarm each relation or index from it */
+        for (int i = 0; i < list_length(oidList); i++)
+        {
+            prewarm_relations(list_nth_oid(oidList, i));
+        }
     }
 
-    // Empty buffer
-    // empty_buffer();
+    /*
+     * COLD START
+     * Get the relations and indices from the query plan tree and prewarm each of them
+     */
+
+    if (strcmp(experiment_mode, "cold") == 0)
+    {
+        /* Empty the buffer */
+    }
 
     return pg_exec_run_prev_hook(queryDesc, direction, count, execute_once);
 }
@@ -115,6 +161,19 @@ static void pg_query_pre_run_hook(QueryDesc *queryDesc, ScanDirection direction,
 
 void _PG_init(void)
 {
+
+    DefineCustomStringVariable(
+        "experiment_mode",
+        "Activate either cold or hot start.",
+        NULL,
+        &experiment_mode,
+        "hot", /* default value */
+        PGC_SUSET,
+        0,
+        NULL,
+        NULL,
+        NULL);
+
     pg_exec_run_prev_hook = ExecutorRun_hook;
 
     if (pg_exec_run_prev_hook == NULL)
